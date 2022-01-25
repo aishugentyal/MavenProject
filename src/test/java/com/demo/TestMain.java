e06f099fffefa0eb2412447152625556a52b229d
Aishwarya Gentyal <aishugentyal2016@gmail.com>
2022-01-25 11:10:40 +0530

    Maven Project Commited

---
+++ src/test/java/com/demo/TestMain.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestMain {
+
+    @Test
+    public void testInputIsEven(){
+        assertTrue(Main.checkIfInputIsAnEvenNumber(23)); // Assertion
+    }
+}
