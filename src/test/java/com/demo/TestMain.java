e81d5951492f92127e66c21994e0cc6392f934a2
aishugentyal <97588010+aishugentyal@users.noreply.github.com>
2022-01-25 11:57:31 +0530

    Update TestMain.java

--- src/test/java/com/demo/TestMain.java
+++ src/test/java/com/demo/TestMain.java
@@ -7,6 +7,6 @@ public class TestMain {
 
     @Test
     public void testInputIsEven(){
-        assertTrue(Main.checkIfInputIsAnEvenNumber(23)); // Assertion
+        assertTrue(Main.checkIfInputIsAnEvenNumber(22)); // Assertion
     }
 }
